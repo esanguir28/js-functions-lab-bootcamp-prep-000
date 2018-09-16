4d299af873c301a9cc9b367e68fd75092704de46
Erica Janine Sanguir <ericajanisan@gmail.com>
2018-09-16 00:46:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday,name){
   return 'Happy Independence Day, you!'
 }
 
-function holidayCountdown(holiday,days){
+function holidayCountdown(string){
   return "It's 20 days until Mother's Day!"
 }
